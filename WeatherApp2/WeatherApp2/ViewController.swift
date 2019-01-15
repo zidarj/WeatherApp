7effdb6c16f4f8a8eaa3904ed20e3e0443215b25
Josip <45390141+zidarj@users.noreply.github.com>
2019-01-15 08:55:58 +0100

    pod init

---
+++ WeatherApp2/WeatherApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp2
+//
+//  Created by Josip Zidar on 15/01/2019.
+//  Copyright © 2019 Josip Zidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
